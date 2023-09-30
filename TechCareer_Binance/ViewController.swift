7dcbd3c93db14d40a7da7006a564bb8a726c1c3f
Hakan Baran <hakanbaran.developer@gmail.com>
2023-10-01 00:59:50 +0300

    First Commit...

---
+++ TechCareer_Binance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechCareer_Binance
+//
+//  Created by Hakan Baran on 1.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
