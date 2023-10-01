85595f55f53996c038235e94775454cd507a5106
Hakan Baran <hakanbaran.developer@gmail.com>
2023-10-01 11:16:31 +0300

    Last Commit...

--- TechCareer_Binance/Views/Cells/CoinCell.swift
+++ TechCareer_Binance/Views/Cells/CoinCell.swift
@@ -30,6 +30,7 @@ class CoinCell: UITableViewCell {
     private let coinPrice: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textAlignment = .right
         label.text = "0.4545"
         label.textColor = .label
         return label
@@ -37,7 +38,8 @@ class CoinCell: UITableViewCell {
     
     private let miniCoinPrice: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 10, weight: .regular)
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textAlignment = .right
         label.text = "0.4545"
         label.textColor = .lightGray
         return label
@@ -45,10 +47,13 @@ class CoinCell: UITableViewCell {
     
     private let coinYuzde: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 10, weight: .regular)
+        label.font = .systemFont(ofSize: 15, weight: .medium)
         label.text = "+%2,54"
+        label.textAlignment = .center
         label.textColor = .white
         label.backgroundColor = UIColor(hex: "#2DBD85")
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 3
         return label
     }()
     
@@ -72,13 +77,24 @@ class CoinCell: UITableViewCell {
         let width = contentView.frame.width
         let height = contentView.frame.height
         
-        coinName.frame = CGRect(x: 0, y: 0, width: width/5, height: height/2)
-        uzunVadeli.frame = CGRect(x: 0, y: height/2 + height/10, width: width/5, height: height/3)
+        coinName.frame = CGRect(x: width/25, y: height/5, width: width/3, height: height/2)
+        uzunVadeli.frame = CGRect(x: width/25, y: height/2 + height/10, width: width/5, height: height/3)
+        
+        coinPrice.frame = CGRect(x: width/2+width/30, y: height/5, width: width/5, height: height/3)
+        miniCoinPrice.frame = CGRect(x: width/2+width/30, y: height/5+height/3, width: width/5, height: height/3)
+        
+        
+        coinYuzde.frame = CGRect(x: width-width/4, y: height/5, width: width/4.5, height: height/1.8)
+        
+        
         
     }
     
-    public func configure(with model: String) {
-        coinName.text = model
+    public func configure(with name: String, price: String, yuzde: String ) {
+        coinName.text = name
+        coinPrice.text = price
+        miniCoinPrice.text = price
+        coinYuzde.text = yuzde
     }
 
 }
--- TechCareer_Binance/Views/ViewControllers/HomePage.swift
+++ TechCareer_Binance/Views/ViewControllers/HomePage.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
+
+struct Coin {
+    let name: String?
+    let price: String?
+    let yuzde: String?
+}
+
+
 class HomePage: UIViewController {
     
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    var coinArray = [Coin]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +29,26 @@ class HomePage: UIViewController {
         tableView.dataSource = self
         
         tableView.register(CoinCell.self, forCellReuseIdentifier: CoinCell.identifier)
+        tableView.separatorStyle = .none
+        
+        coinArrayAppand()
+    }
+    
+    func coinArrayAppand() {
+        
+        let coin1 = Coin(name: "BTCUSDT", price: "27.115,51", yuzde: "+%0,70")
+        let coin2 = Coin(name: "ETHUSDT", price: "1.678,83", yuzde: "+%0,39")
+        let coin3 = Coin(name: "1INCHUSDT", price: "0,2704", yuzde: "+%0,71")
+        let coin4 = Coin(name: "KAVAUSDT", price: "0,638", yuzde: "+%1.43")
+        let coin5 = Coin(name: "SXPUSDT", price: "0,2904", yuzde: "+%0,94")
+        let coin6 = Coin(name: "COMPUSDT", price: "47.39", yuzde: "+1.76")
+        
+        coinArray.append(coin1)
+        coinArray.append(coin2)
+        coinArray.append(coin3)
+        coinArray.append(coin4)
+        coinArray.append(coin5)
+        coinArray.append(coin6)
     }
 }
 
@@ -30,11 +60,18 @@ extension HomePage: UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return coinArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CoinCell.identifier, for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CoinCell.identifier, for: indexPath) as? CoinCell else {
+            return UITableViewCell()
+        }
+        
+        
+        let model = coinArray[indexPath.row]
+        
+        cell.configure(with: model.name ?? "", price: model.price ?? "", yuzde: model.yuzde ?? "")
         
         return cell
     }
